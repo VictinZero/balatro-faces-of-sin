d325c6c87d3f60e563eef8a29630e3a4a9672c83
VictinZero <79382302+VictinZero@users.noreply.github.com>
2024-11-25 22:22:10 +0100

    Use SMODS.DeckSkin

--- facesofsin.lua
+++ facesofsin.lua
@@ -2,28 +2,72 @@
 --- MOD_NAME: Faces of Sin
 --- MOD_ID: fos
 --- MOD_AUTHOR: [TiltedHat, Victin]
---- MOD_DESCRIPTION: Texture mod
+--- MOD_DESCRIPTION: Texture mod. Art by TiltedHat. Code by Victin.
 --- LOADER_VERSION_GEQ: 1.0.0
 --- VERSION: 1.0.0
 --- BADGE_COLOR: FF4CA1
 
-SMODS.Atlas {  
-    key = 'cards_1',
+SMODS.Atlas{  
+    key = 'fos_deck',
     px = 71,
     py = 95,
-    path = '8BitDeck.png',
-    raw_key = true,
+    path = 'deck.png',
+    prefix_config = {key = false},
 }
 
-SMODS.Atlas {  
-    key = 'cards_2',
-    px = 71,
-    py = 95,
-    path = '8BitDeck.png',
-    raw_key = true,
+SMODS.DeckSkin{
+    key = "hearts_skin",
+    suit = 'Hearts',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
+    },
+    lc_atlas = "fos_deck",
+    loc_txt = {
+        ['en-us'] = 'Faces of Sin'
+    },
+    posStyle = 'deck'
 }
 
-SMODS.Atlas {  
+SMODS.DeckSkin{
+    key = "clubs_skin",
+    suit = 'Clubs',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
+    },
+    lc_atlas = "fos_deck",
+    loc_txt = {
+        ['en-us'] = 'Faces of Sin'
+    },
+    posStyle = 'deck'
+}
+
+SMODS.DeckSkin{
+    key = "diamonds_skin",
+    suit = 'Diamonds',
+    ranks = {
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
+    },
+    lc_atlas = "fos_deck",
+    loc_txt = {
+        ['en-us'] = 'Faces of Sin'
+    },
+    posStyle = 'deck'
+}
+
+SMODS.DeckSkin{
+    key = "spades_skin",
+    suit = 'Spades',
+    ranks = {
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace"
+    },
+    lc_atlas = "fos_deck",
+    loc_txt = {
+        ['en-us'] = 'Faces of Sin'
+    },
+    posStyle = 'deck'
+}
+
+SMODS.Atlas{  
     key = 'modicon',
     px = 32,
     py = 32,
