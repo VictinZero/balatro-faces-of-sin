30c83cbe0a6f80952844f6b24109b324724553e6
VictinZero <79382302+VictinZero@users.noreply.github.com>
2024-11-29 17:49:46 +0100

    Refactor code

--- facesofsin.lua
+++ facesofsin.lua
@@ -5,68 +5,37 @@
 --- MOD_AUTHOR: [TiltedHat, Victin]
 --- MOD_DESCRIPTION: Texture mod. Art by TiltedHat. Code by Victin.
 --- LOADER_VERSION_GEQ: 1.0.0
---- VERSION: 1.0.0
+--- VERSION: 1.1.2
 --- BADGE_COLOR: FF4CA1
 
+local atlas_key = 'fos_deck' -- Format: PREFIX_KEY
+local atlas_path = 'deck.png' -- Filename for the image in the asset folder
+
+local suits = {'hearts', 'clubs', 'diamonds', 'spades'} -- Which suits to replace
+local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",} -- Which ranks to replace
+
+local description = 'Faces of Sin' -- English-language description, also used as default
+
 SMODS.Atlas{  
-    key = 'fos_deck',
+    key = atlas_key,
     px = 71,
     py = 95,
-    path = 'deck.png',
+    path = atlas_path,
     prefix_config = {key = false},
 }
 
-SMODS.DeckSkin{
-    key = "hearts_skin",
-    suit = 'Hearts',
-    ranks = {
-        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
-    },
-    lc_atlas = "fos_deck",
-    loc_txt = {
-        ['en-us'] = 'Faces of Sin'
-    },
-    posStyle = 'deck'
-}
-
-SMODS.DeckSkin{
-    key = "clubs_skin",
-    suit = 'Clubs',
-    ranks = {
-        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
-    },
-    lc_atlas = "fos_deck",
-    loc_txt = {
-        ['en-us'] = 'Faces of Sin'
-    },
-    posStyle = 'deck'
-}
-
-SMODS.DeckSkin{
-    key = "diamonds_skin",
-    suit = 'Diamonds',
-    ranks = {
-        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",
-    },
-    lc_atlas = "fos_deck",
-    loc_txt = {
-        ['en-us'] = 'Faces of Sin'
-    },
-    posStyle = 'deck'
-}
-
-SMODS.DeckSkin{
-    key = "spades_skin",
-    suit = 'Spades',
-    ranks = {
-    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace"
-    },
-    lc_atlas = "fos_deck",
-    loc_txt = {
-        ['en-us'] = 'Faces of Sin'
-    },
-    posStyle = 'deck'
-}
+for _, suit in ipairs(suits) do
+    SMODS.DeckSkin{
+        key = suit.."_skin",
+        suit = suit:gsub("^%l", string.upper),
+        ranks = ranks,
+        lc_atlas = atlas_key,
+        loc_txt = {
+            ['en-us'] = description
+        },
+        posStyle = 'deck'
+    }
+end
 
 SMODS.Atlas{  
     key = 'fos_modicon',
